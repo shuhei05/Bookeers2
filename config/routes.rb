1f30962b998f8bd688643dbd325e41bd20f699be
shuhei05 <qoo0228y@gmail.com>
2021-05-01 14:18:01 +0000

    いいね実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
-  root to: 'homes#top'
+    root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
   
 end
