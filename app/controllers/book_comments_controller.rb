f6de4930819b2883dd7890fe8dc0bc75f69e9897
shuhei05 <qoo0228y@gmail.com>
2021-05-04 09:09:51 +0000

    課題6終了

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,18 @@
 class BookCommentsController < ApplicationController
     
   def create
-      book = Book.find(params[:book_id])
+      @book = Book.find(params[:book_id]) #コメントする投稿ID
       comment = BookComment.new(book_comment_params)
       comment.user_id = current_user.id
-      comment.book_id = book.id
+      comment.book_id = @book.id #数字がわかるようにidを記述。なくても作動するがわかりやすくするため。
       comment.save
-      redirect_to book_path(book)
   end
 
   def destroy
-      BookComment.find_by(id: params[:id],book_id: params[:book_id]).destroy
-      redirect_to book_path(params[:book_id])
+    
+    BookComment.find_by(id: params[:id],book_id: params[:book_id]).destroy
+    @book = Book.find(params[:book_id])
+
   end
   
    private
