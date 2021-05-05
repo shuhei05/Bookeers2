5e126e12318d8b67a46b86b1d15c4d6ea3eed692
shuhei05 <qoo0228y@gmail.com>
2021-05-05 09:52:47 +0000

    課題7終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,4 +36,16 @@ class User < ApplicationRecord
       following_user.include?(user)
     end
     
+    #都道府県
+    include JpPrefecture
+    jp_prefecture :prefecture_code
+
+    def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+    end
+
+    def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_02_070949) do
+ActiveRecord::Schema.define(version: 2021_05_05_091816) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -54,6 +54,11 @@ ActiveRecord::Schema.define(version: 2021_05_02_070949) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.integer "postcode"
+    t.integer "prefecture_code"
+    t.string "address_city"
+    t.string "address_street"
+    t.string "address_building"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
